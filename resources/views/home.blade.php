bc4c7154183ffc17f450de2bc7cb072fb437fb4b
Djhuwae <djhuwae@avans.nl>
2018-03-11 19:38:03 +0100

    small fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                         @foreach($productChunk as $product)
                             <div class="col-lg-4 col-md-6 mb-4">
                                 <div class="card h-100">
-                                    <a href="#"><img class="card-img-top img-fluid" src={{$product->imageurl }} alt=""></a>
+                                    <a href="#"><img class="card-img-top img-fluid productimg" src={{$product->imageurl }} alt=""></a>
                                     <div class="card-block">
                                         <h4 class="card-title"><a href="#">{{$product->name }}</a></h4>
                                         <h5>$ {{ $product->price }}</h5>
