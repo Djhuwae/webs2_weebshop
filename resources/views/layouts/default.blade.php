16bfd94049ab42ca052bccf1f329eaf4a3d4c95c
Donneh Huwae <djhuwae@avans.nl>
2018-03-15 14:45:53 +0100

    Added the about page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -136,7 +136,7 @@
 <!-- Footer -->
 <footer class="py-5 bg-inverse">
     <div class="container">
-        <p class="m-0 text-center text-white">&copy; Copyright 2018. Powered by Donnèh en Nur. ALL RIGHTS RESERVED</p>
+        <p class="m-0 text-center text-white">&copy; Copyright 2018. Powered by Weebshop. ALL RIGHTS RESERVED</p>
     </div>
     <!-- /.container -->
 </footer>
